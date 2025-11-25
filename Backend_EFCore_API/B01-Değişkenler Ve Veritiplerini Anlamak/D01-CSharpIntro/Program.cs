4299b952bfccfb8ac406f9aa0f780df6a1d8cf7c
Alperen Pişkin <alperen013@outlook.com.tr>
2025-11-25 14:00:21 +0300

    Değişkenler Ve Veritipleri
    Bu ilk bölüm ve giriş kısmıdır

---
+++ Backend_EFCore_API/B01-Değişkenler Ve Veritiplerini Anlamak/D01-CSharpIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace D01_CSharpIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double aboneSayisi = 34.5;
+            int yas = 22;
+
+
+            string duyuru = "Duyurumuz var";
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
