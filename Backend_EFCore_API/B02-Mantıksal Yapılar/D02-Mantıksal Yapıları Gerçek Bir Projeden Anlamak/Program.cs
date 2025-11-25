bb4860f4296457fde69ec2f35a79d577bb9ea912
Alperen Pişkin <alperen013@outlook.com.tr>
2025-11-25 14:55:23 +0300

    Mantıksal Yapılar

---
+++ Backend_EFCore_API/B02-Mantıksal Yapılar/D02-Mantıksal Yapıları Gerçek Bir Projeden Anlamak/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace D02_Mantıksal_Yapıları_Gerçek_Bir_Projeden_Anlamak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool deger = true;
+
+            double piyasaDun = 115000.6;
+            double piyasaBugun = 116000.6;
+            bool sonuc = piyasaBugun == piyasaDun;
+
+            string mesajArtis = "Artış oku";
+            string mesajAzalis = "Azalış oku";
+            string mesajAyni = "Sabit oku";
+            if (piyasaBugun>piyasaDun)
+            {
+                Console.WriteLine(mesajArtis);
+            }
+            else if (piyasaBugun==piyasaDun) 
+            {
+                Console.WriteLine(mesajAyni);
+            }
+            else
+            {
+                Console.WriteLine(mesajAzalis);
+            }
+        }
+    }
+}
---
+++ Backend_EFCore_API/B02-Mantıksal Yapılar/D03-Karar Yapıları Ödevi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Channels;
+
+namespace D03_Karar_Yapıları_Ödevi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 15;
+            int sayi2 = 25;
+
+            string SayiKontrol =
+            (sayi1 > sayi2) ? "sayi1 büyüktür sayi2'den.." :
+            (sayi2 > sayi1) ? "sayi2 büyüktür sayi1'den.." :
+            "sayi1 eşittir sayi2'ye..";
+            Console.WriteLine(SayiKontrol);
+        }
+    }
+}
